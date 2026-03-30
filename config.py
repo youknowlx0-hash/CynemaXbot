26f179718ac71dd00551c0fcca7e9885c988e1ca
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 13:23:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ CHANNELS = ["@CynemaXOfficial","@H3xShadow"]
 ADMIN_ID = 6813806104
 
 WEBSITE_URL = "https://www.instagram.com/h3xshadow"
-MOVIES_URL = "https://yourmovielink.com"   # 🎬 yaha apna link daal
+MOVIES_URL = "https://t.me/Manish_Looterss"   # 🎬 yaha apna link daal
 START_IMG = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 
 VIDLINK_BASE = "https://vidlink.pro/movie/"
