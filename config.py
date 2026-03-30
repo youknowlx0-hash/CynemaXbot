0afafead02ac8d28c0e5ee0c6e52ab05c2828d38
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 09:07:54 +0530

    Update POSTER_URL to new image link

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 
 BOT_TOKEN = "8115145972:AAHihp5gbuTmrSxE_XPcMjBkbp41qeTehXE"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
-POSTER_URL = "https://link-to-your-poster-image.jpg"
+POSTER_URL = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 
 CHANNELS = ["@H3xShadow", "@CynemaXOfficial"]  # Force join channels
