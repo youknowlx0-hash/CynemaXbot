556786d20042fbde3010f7edc26678013f69e133
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 13:07:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ BOT_TOKEN = "8115145972:AAFN_N8Z-s3g0Z_PmZ0u6QihdgQl_GHDskk"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 
 CHANNELS = ["@CynemaXOfficial","@H3xShadow"]
-ADMIN_ID = 7702942505
+ADMIN_ID = 6813806104
 
 WEBSITE_URL = "https://www.instagram.com/h3xshadow"
 
