65d826cfeea02398ad6aae37cb98602794c7848e
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 10:21:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 POSTER_URL = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 INSTAGRAM_URL = "https://www.instagram.com/h3xshadow?"
 CHANNELS = ["@CynemaXOfficial", "@H3xShadow"]  # Force join channels
+ADMIN_ID = 6813806104
+VIDLINK_BASE = "https://vidlink.pro/movie/"
