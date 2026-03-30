c5d283a3f6e72d25f98865348d61e32343fab879
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 11:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 
-BOT_TOKEN = "8115145972:AAFrfCK4aOA05SH9Sr981SJqTGHoyMhtDi0"
+BOT_TOKEN = "8115145972:AAFN_N8Z-s3g0Z_PmZ0u6QihdgQl_GHDskk"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 POSTER_URL = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 INSTAGRAM_URL = "https://www.instagram.com/h3xshadow?"
 CHANNELS = ["@CynemaXOfficial", "@H3xShadow"]  # Force join channels
 ADMIN_ID = 6813806104
 VIDLINK_BASE = "https://vidlink.pro/movie/"
-MOVIE_FILE_BASE = "https://drive.google.com/uc?export=download&id="
