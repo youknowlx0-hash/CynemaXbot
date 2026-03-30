f3d330ddac8d8c448633c2992673b16bab540ea5
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 09:04:42 +0530

    Add POSTER_URL and adjust CHANNELS definition

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
+# config.py
+
 BOT_TOKEN = "8115145972:AAHihp5gbuTmrSxE_XPcMjBkbp41qeTehXE"
-ADMIN_ID = 6813806104
-CHANNELS = ["@H3xShadow", "@CynemaXOfficial"]
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
+POSTER_URL = "https://link-to-your-poster-image.jpg"
+
+CHANNELS = ["@H3xShadow", "@CynemaXOfficial"]  # Force join channels
