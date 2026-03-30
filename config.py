64fac3ed61509d9f8170a210b6d273c07cfaacf3
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 09:24:21 +0530

    Update BOT_TOKEN and rearrange CHANNELS

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-# config.py
 
-BOT_TOKEN = "8115145972:AAHihp5gbuTmrSxE_XPcMjBkbp41qeTehXE"
+BOT_TOKEN = "8115145972:AAFrfCK4aOA05SH9Sr981SJqTGHoyMhtDi0"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 POSTER_URL = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
-
-CHANNELS = ["@H3xShadow", "@CynemaXOfficial"]  # Force join channels
+INSTAGRAM_URL = "https://www.instagram.com/h3xshadow?"
+CHANNELS = ["@CynemaXOfficial", "@H3xShadow"]  # Force join channels
