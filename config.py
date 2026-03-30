8bc438a3c4b123bc0c059371de6684570a29e002
youknowlx0-hash <youknowlx0@gmail.com>
2026-03-30 13:17:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 BOT_TOKEN = "8115145972:AAFN_N8Z-s3g0Z_PmZ0u6QihdgQl_GHDskk"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 
@@ -6,9 +5,10 @@ CHANNELS = ["@CynemaXOfficial","@H3xShadow"]
 ADMIN_ID = 6813806104
 
 WEBSITE_URL = "https://www.instagram.com/h3xshadow"
+MOVIES_URL = "https://yourmovielink.com"   # 🎬 yaha apna link daal
+START_IMG = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 
 VIDLINK_BASE = "https://vidlink.pro/movie/"
-START_IMG = "https://i.ibb.co/zHhfjxBf/file-103.jpg" 
 
 START_SEARCH = 3
 REF_BONUS = 3
