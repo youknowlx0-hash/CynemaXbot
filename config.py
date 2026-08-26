ef2d567fd8c5f35f01de99f63555c9524b22aa82
youknowlx0-hash <youknowlx0@gmail.com>
2026-08-26 17:16:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-BOT_TOKEN = "8115145972:AAFN_N8Z-s3g0Z_PmZ0u6QihdgQl_GHDskk"
+BOT_TOKEN = "8990984656:AAHQoDujHrKtEDGuR5wRJRRsfXv5V5eI36Y"
 TMDB_API_KEY = "ce822ebcfcfc1f92264713bb4306fdbd"
 
-CHANNELS = ["@CynemaXOfficial","@H3xShadow"]
-ADMIN_ID = 6813806104
+CHANNELS = ["@VenomTricksOfficial","@webSeriesUpdater"]
+ADMIN_ID = 7702942505
 
-WEBSITE_URL = "https://www.instagram.com/h3xshadow"
-MOVIES_URL = "https://t.me/Manish_Looterss"   # 🎬 yaha apna link daal
+WEBSITE_URL = "https://www.instagram.com/pvtluckylx?"
+MOVIES_URL = ""   # 🎬 yaha apna link daal
 START_IMG = "https://i.ibb.co/zHhfjxBf/file-103.jpg"
 
 VIDLINK_BASE = "https://vidlink.pro/movie/"
